5bf43c3d4c952b39ec0df85a820dd56927d9a04a
Damla <damlakayali@gmail.com>
2020-11-15 18:49:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function ClickButton(id){
+    var link="";
+    switch(id){
+        case 1:
+            window.location.href="https://www.damlakayali.net/web-gelistiricisinin-yol-haritasi";
+            break;
+        case 2:
+            window.location.href="https://www.damlakayali.net/yazilim-muhendisligi-en-cok-gelen-sorular-1/";
+            break;
+        case 3:
+            window.location.href="https://www.damlakayali.net/mimarliktan-yazilima/";
+            break;
+        case 4:
+            window.location.href="https://www.damlakayali.net/category/asp-net-mvc/";
+            break;
+        case 5:
+            window.location.href="https://www.damlakayali.net/ilk-is-tecrubem/";
+            break;  
+    }
+}
